1ff572aa9323d2d66504a1aa06dc220683e702e5
Sathvik Bennihalli <sathvikbennihalli@Sathviks-MacBook-Air.local>
2024-03-01 16:01:11 +0530

    header and footer

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../App.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Simple React Website</h1>
+    </header>
+  );
+}
+
+export default Header;
